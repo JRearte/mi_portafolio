7bd580482d8830f80f579d6f698e6c192d62703b
Jonatan Rearte <jrearte198@gmail.com>
2025-12-01 01:14:12 -0300

    actualizacion de datos academicos

--- src/data/academico.ts
+++ src/data/academico.ts
@@ -1,35 +1,68 @@
 import { FaBrain, FaProjectDiagram, FaCodeBranch, FaUserCheck } from "react-icons/fa";
 import { GiGraduateCap, GiNotebook } from "react-icons/gi";
 import { HiOutlineDocumentText, HiOutlineClipboardDocumentCheck, HiOutlineCpuChip } from "react-icons/hi2";
-import { TbCertificate, TbTopologyStar3, TbBrain } from "react-icons/tb";
+import { TbCertificate, TbTopologyStar3, TbBrain, TbNumber10 } from "react-icons/tb";
+import { RiNumber7, RiNumber8, RiNumber9, RiNumber5, RiNumber6, RiNumber4 } from "react-icons/ri";
 import { PiDatabase } from "react-icons/pi";
 import { RiProjectorLine, RiBugLine } from "react-icons/ri";
 import { MdOutlineLan, MdOutlineMemory, MdDeviceHub } from "react-icons/md";
 import { IoTimeOutline } from "react-icons/io5";
 import { FiTarget } from "react-icons/fi";
 
+
 export const sumarios = [
+  {
+    titulo: "Tecnicatura en Análisis de Sistemas",
+    icono: GiGraduateCap,
+    items: [
+      { texto: "Idioma Moderno (Inglés)", icono: RiNumber7, title: "Materia finalizada el 25-09-2018" },
+      { texto: "Matemática 1", icono: RiNumber8, title: "Materia finalizada el 11-07-2022" },
+      { texto: "Resolución de Problemas y Algoritmos", icono: RiNumber9, title: "Materia finalizada el 07-12-2018" },
+      { texto: "Aspectos Profesionales", icono: RiNumber9, title: "Materia finalizada el 26-09-2019" },
+      { texto: "Proceso de Desarrollo de Software", icono: RiNumber5, title: "Materia finalizada el 05-12-2018" },
+      { texto: "Matemática Discreta", icono: RiNumber4, title: "Materia finalizada el 27-02-2023" },
+      { texto: "Organización de las Computadoras", icono: RiNumber9, title: "Materia finalizada el 11-07-2018" },
+      { texto: "Matemática 2", icono: RiNumber7, title: "Materia finalizada el 19-12-2022" },
+      { texto: "Programación de Algoritmos", icono: RiNumber8, title: "Materia finalizada el 07-12-2018" },
+      { texto: "Programación Orientada a Objetos", icono: RiNumber9, title: "Materia finalizada el 13-10-2020" },
+      { texto: "Gestión de Organizaciones", icono: RiNumber8, title: "Materia finalizada el 30-07-2019" },
+      { texto: "Arquitecturas de Computadoras", icono: RiNumber6, title: "Materia finalizada el 04-12-2018" },
+      { texto: "Requerimientos de Software", icono: RiNumber8, title: "Materia finalizada el 19-12-2019" },
+      { texto: "Estructura de Datos", icono: RiNumber9, title: "Materia finalizada el 03-12-2024" },
+      { texto: "Base de Datos", icono: RiNumber9, title: "Materia finalizada el 14-02-2022" },
+      { texto: "Análisis y Diseño de Software", icono: RiNumber8, title: "Materia finalizada el 04-08-2023" },
+      { texto: "Sistemas Operativos", icono: RiNumber9, title: "Materia finalizada el 10-07-2019" },
+      { texto: "Validación y Verificación de Software", icono: RiNumber7, title: "Materia finalizada el 27-02-2024" },
+      { texto: "Gestión de Proyectos de Software", icono: RiNumber7, title: "Materia finalizada el 10-07-2024" },
+      { texto: "Laboratorio de Programación", icono: RiNumber8, title: "Materia finalizada el 25-09-2023" },
+      { texto: "Estadística", icono: RiNumber8, title: "Materia finalizada el 20-03-2024" },
+      { texto: "Redes y Telecomunicaciones", icono: RiNumber9, title: "Materia finalizada el 15-12-2020" },
+      { texto: "Sistemas Operativos Distribuidos", icono: RiNumber8, title: "Materia finalizada el 24-02-2021" },
+      { texto: "Fundamentos de Ciencias de la Computación", icono: RiNumber9, title: "Materia finalizada el 10-07-2023" },
+      { texto: "Laboratorio de Desarrollo de Software", icono: TbNumber10, title: "Materia finalizada el 30-04-2025" }
+    ]
+  },
   {
     titulo: "Formación Académica",
     icono: GiGraduateCap,
     items: [
-        { texto: "Analista de Sistemas - Finalizado en 2025", icono: GiGraduateCap, title: "Título universitario" },
-        { texto: "Proyecto de innovación tecnológica 1.0 – 2024", icono: FaProjectDiagram, title: "Proyecto universitario de innovación tecnológica" },
-        { texto: "Formación integral en extensión: hacia el fortalecimiento de la función social de la universidad - 2024", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Diseño de pósters accesibles para divulgación de acciones de extensión e investigación - 2024", icono: TbCertificate, title: "Taller de accesibilidad" },
-        { texto: "Hacer y pensar una universidad para todos - 2023", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Protocolo de circulación de la UNPA ante la emergencia sanitaria por COVID-19 - 2022", icono: TbCertificate, title: "Capacitación institucional" },
-        { texto: "Usabilidad de sitios de gobierno - 2022", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Interfaz web para sistemas embebidos - 2022", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Terminal de autogestión y revelador de puentes - 2021", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Redes de datos hogareñas: elementos básicos - 2021", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Metodología de diseño de redes: Top Down - 2021", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Aprendiendo a hacer streaming en la Patagonia Austral - 2021", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Game Development: introducción a Unity - 2019", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Hardware y reparación de computadoras - 2019", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Comunicación efectiva - 2019", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Redes de área local: instalación y configuración - 2018", icono: TbCertificate, title: "Curso certificado" },
-        { texto: "Maquetación web con HTML5 y CSS3 - 2018", icono: TbCertificate, title: "Curso certificado" }
+      { texto: "Analista de Sistemas - Finalizado en 2025", icono: GiGraduateCap, title: "Título universitario" },
+      { texto: "Proyecto de innovación tecnológica 1.0 – 2024", icono: FaProjectDiagram, title: "Proyecto universitario de innovación tecnológica" },
+      { texto: "Formación integral en extensión: hacia el fortalecimiento de la función social de la universidad - 2024", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Diseño de pósters accesibles para divulgación de acciones de extensión e investigación - 2024", icono: TbCertificate, title: "Taller de accesibilidad" },
+      { texto: "Hacer y pensar una universidad para todos - 2023", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Protocolo de circulación de la UNPA ante la emergencia sanitaria por COVID-19 - 2022", icono: TbCertificate, title: "Capacitación institucional" },
+      { texto: "Usabilidad de sitios de gobierno - 2022", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Interfaz web para sistemas embebidos - 2022", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Terminal de autogestión y revelador de puentes - 2021", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Redes de datos hogareñas: elementos básicos - 2021", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Metodología de diseño de redes: Top Down - 2021", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Aprendiendo a hacer streaming en la Patagonia Austral - 2021", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Game Development: introducción a Unity - 2019", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Hardware y reparación de computadoras - 2019", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Comunicación efectiva - 2019", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Redes de área local: instalación y configuración - 2018", icono: TbCertificate, title: "Curso certificado" },
+      { texto: "Maquetación web con HTML5 y CSS3 - 2018", icono: TbCertificate, title: "Curso certificado" }
     ]
   },
   {
